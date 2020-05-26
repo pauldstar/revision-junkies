01ee0fcfdf794c2df3515ac179969c48afc6b0fe
Paul <Ogbeiwi>
2020-05-26 12:54:15 +0100

    add searchbar

--- resources/js/components/dashboard/Header.vue
+++ resources/js/components/dashboard/Header.vue
@@ -1,57 +1,9 @@
 <template>
-    <div class="header">
+    <div class="header dashboard pt-2 pb-2 mt-1">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xl-12">
-                    <nav class="navbar navbar-expand-lg px-0 justify-content-between">
-                        <logo :responsive="true"/>
-
-                        <div class="dashboard_log my-2">
-                            <div class="d-flex align-items-center">
-
-                                <div class="account_money d-none d-sm-block">
-                                    <ul class="d-flex">
-                                        <li class="crypto">
-                                            <span>0.0025</span>
-                                            <i class="cc BTC-alt"></i>
-                                        </li>
-                                        <li class="usd">
-                                            <span>19.93 USD</span>
-                                        </li>
-                                    </ul>
-                                </div>
-
-                                <div class="profile_log dropdown">
-                                    <div class="user" @click="show = !show">
-                                        <span class="thumb"><i class="la la-user"></i></span>
-                                        <span class="name ml-2">Maria Pascle</span>
-                                        <span class="arrow ml-2"><i class="la la-angle-down"></i></span>
-                                    </div>
-                                    <div
-                                        class="dropdown-menu dropdown-menu-right"
-                                        :class="{show: show}"
-                                        @click.capture="show = !show"
-                                    >
-                                        <router-link to="/dashboard/accounts" class="dropdown-item">
-                                            <i class="la la-user"></i> Account
-                                        </router-link>
-                                        <router-link to="/dashboard/history" class="dropdown-item">
-                                            <i class="la la-book"></i> History
-                                        </router-link>
-                                        <router-link to="/dashboard/settings" class="dropdown-item">
-                                            <i class="la la-cog"></i> Setting
-                                        </router-link>
-                                        <router-link to="/lock" class="dropdown-item">
-                                            <i class="la la-lock"></i> Lock
-                                        </router-link>
-                                        <router-link to="/sign-in" class="dropdown-item logout">
-                                            <i class="la la-sign-out"></i> Logout
-                                        </router-link>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </nav>
+                    <search-bar/>
                 </div>
             </div>
         </div>
@@ -60,14 +12,13 @@
 
 <script>
     import Logo from "@app/components/Logo";
+    import SearchBar from "@app/components/dashboard/SearchBar";
 
     export default {
-        components: {Logo},
+        components: {SearchBar, Logo},
 
-        data() {
-            return {
-                show: false
-            }
-        }
+        data: _=> ({
+            show: false
+        })
     }
 </script>
---
+++ resources/js/components/dashboard/SearchBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="input-group input-group-seamless-append">
+        <input
+            type="text"
+            id="searchbar-dbh"
+            class="form-control"
+            ref="search"
+            v-model="search"
+        >
+
+        <logo
+            v-if="!search"
+            class="placeholder-logo"
+            :responsive="true"
+        />
+
+        <div class="input-group-append">
+            <div
+                id="search-icon-dbh"
+                class="input-group-text bg-white"
+                @click="focusSearch"
+            >
+                <i class="fa fa-search"></i>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Logo from '@app/components/Logo';
+
+    export default {
+        components: {Logo},
+
+        data: _=> ({
+            search: ''
+        }),
+
+        methods: {
+            focusSearch() {
+                this.$refs.search.focus();
+            }
+        }
+    }
+</script>
