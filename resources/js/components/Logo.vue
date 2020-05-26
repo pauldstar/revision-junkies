05e0f2149727e9070c7bdf3030465e38fec6104f
Paul <Ogbeiwi>
2020-05-26 12:55:04 +0100

    update logo.vue

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -1,17 +1,21 @@
 <template>
-    <router-link  to="/dashboard" class="d-flex logo">
+    <component
+        :is="logoWrapper"
+        to="/dashboard"
+        class="d-flex logo"
+    >
         <template v-if="responsive">
             <span class="reverse-text">Z</span>
             <span class="d-sm-none ml-n1">Z</span>
             <span class="d-none d-sm-block">tudytipZ</span>
         </template>
         <template v-else-if="small">
-            <span class="reverse-text mr-n3">Z</span>Z
+            <span class="reverse-text mr-n1">Z</span>Z
         </template>
         <template v-else>
             <span class="reverse-text">Z</span>tudytipZ
         </template>
-    </router-link>
+    </component>
 </template>
 
 <script>
@@ -19,6 +23,13 @@
         props: {
             responsive: {type: Boolean, default: false},
             small: {type: Boolean, default: true},
+            link: {type: Boolean, default: true}
+        },
+
+        computed: {
+            logoWrapper() {
+                return this.link ? 'router-link' : 'div';
+            }
         }
     }
 </script>
