4c38031c440c1535bb2ef185aebadf1870da32db
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-10-15 21:35:45 +0100

    Update PageTitle.vue
    rename page_title to page-title

--- resources/js/components/dashboard/PageTitle.vue
+++ resources/js/components/dashboard/PageTitle.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="page_title px-3">
+    <div class="page-title px-3">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xl-12">
-                    <div class="page_title-content">
+                    <div class="page-title-content">
                         <p>
                             Welcome Back,<span> Maria Pascle</span>
                         </p>
