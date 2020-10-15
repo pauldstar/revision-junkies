52af8aa34af8eeea396dc30267394d0c80b09bf1
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-10-15 21:41:39 +0100

    add logo component to form-page

--- resources/js/components/FormPage.vue
+++ resources/js/components/FormPage.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="authentication section-padding">
-        <div class="container h-100">
+        <div class="container-fluid h-100">
             <div class="row justify-content-center h-100 align-items-center">
-                <div class="col-xl-5 col-md-6">
-                    <div class="mini-logo text-center my-5">
-                        <router-link to="/"><img src="/images/m_logo.png" alt=""></router-link>
+                <div class="col-12 col-xl-8">
+
+                    <div v-if="logo" class="mini-logo text-center my-5">
+                        <logo class="h1"/>
                     </div>
+
                     <div class="auth-form card">
 
-                        <div v-if="$slots['header']" class="card-header justify-content-center">
+                        <div v-if="$slots['header']" class="card-header justify-content-center px-3">
                             <slot name="header"/>
                         </div>
 
-                        <div class="card-body">
+                        <div class="card-body px-3">
                             <slot/>
                         </div>
 
-                        <div v-if="$slots['footer']" class="card-footer justify-content-center">
+                        <div v-if="$slots['footer']" class="card-footer justify-content-center px-3">
                             <slot name="footer"/>
                         </div>
 
@@ -26,3 +28,16 @@
         </div>
     </div>
 </template>
+
+<script>
+    import logo from '@js/components/Logo';
+
+    export default {
+        components: {logo},
+
+        props: {
+            logo: {type: Boolean, default: true},
+            large: {type: Boolean, default: false},
+        }
+    }
+</script>
