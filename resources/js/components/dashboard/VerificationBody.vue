9cb18dbe0805c22c6c39ca1a3ce7568219450c4d
Paul Ogbeiwi <paulogbeiwi@gmail.com>
2020-10-15 21:39:02 +0100

    Update VerificationBody.vue
    add horizontal padding to card

--- resources/js/components/dashboard/VerificationBody.vue
+++ resources/js/components/dashboard/VerificationBody.vue
@@ -3,17 +3,17 @@
         <div class="container h-100">
             <div class="row justify-content-center h-100 align-items-center">
                 <div class="col-xl-5 col-md-6">
-                    <div class="auth-form card">
+                    <div class="card">
 
                         <div v-if="$slots['header']" class="card-header">
                             <slot name="header"/>
                         </div>
 
-                        <div class="card-body">
+                        <div class="card-body px-3">
                             <slot/>
                         </div>
 
-                        <div v-if="$slots['footer']" class="card-footer justify-content-center">
+                        <div v-if="$slots['footer']" class="card-footer justify-content-center px-3">
                             <slot name="footer"/>
                         </div>
 
